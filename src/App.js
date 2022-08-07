07390865e21da6d2b86b8e4dd53b7582400b5578
Shawlycode <kutinaorlando@gmail.com>
2022-08-07 19:53:10 +0000

    created components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Details from "./components/details/Details";
+import Header from "./components/header/header";
+import Main from "./components/main/Main";
+import Partner from "./components/partner/Partner";
+import Service from "./components/services/Service";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Partner />
+      <Service />
+      <Details />
     </div>
   );
 }
